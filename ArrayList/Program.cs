ddf09c0473298d533bcdbdb6464ef6d4cb3b7bd1
Ubitroll <stephencartner@hotmail.co.uk>
2023-02-10 13:57:44 +0000

    Lesson 7 Excercise

---
+++ ArrayList/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ArrayList
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var Numbers = new List<int>();
+            Random r = new Random();
+
+            for (int x = 0; x < 10; x++)
+            {
+                int num = r.Next(0, 100);
+                Numbers.Add(num);
+            }
+
+            foreach (int num in Numbers)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"Average of random numbers is: {Numbers.Average()}");
+            Numbers.Insert(10, 0);
+            var index = Numbers.IndexOf(0);
+
+            Console.WriteLine($"Index of position 0 is {index}");
+
+            var count = Numbers.Count();
+            Console.WriteLine($"Count is: {count}");
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("Collections using List of Objects");
+            var Student = new List<Person>
+            {
+                new Person() { FirstName = "Joe", LastName = "Bloggs", age = 23},
+                new Person() { FirstName = "Steve", LastName = "May", age = 22},
+                new Person() { FirstName = "Jack", LastName = "Jones", age = 24},
+                new Person() { FirstName = "Tom", LastName = "Jones", age = 23}
+            };
+
+            Console.WriteLine($"Number of objects is: {Student.Count()}");
+
+            foreach (var student in Student)
+            {
+                Console.WriteLine(student.FirstName);
+                Console.WriteLine(student.LastName);
+                Console.WriteLine(student.age);
+                Console.WriteLine("---------------------------");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson7Exercise/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Lesson7Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var objs = new List<object>();
+            objs.Add(2.5d);
+            objs.Add(2.0d);
+            objs.Add(3);
+            objs.Add(2);
+            objs.Add("Hello ");
+            objs.Add("world");
+
+            double sum = 0;
+            int total = 0;
+            string sentance = "";
+
+            Console.WriteLine("Produce a sum of objects");
+            Console.WriteLine("Add ints to ints, floats to floats and concatenate strings");
+            
+            Console.WriteLine("---Print out type---");
+
+            foreach (var obj in objs)
+            {
+                Console.WriteLine(obj.GetType());
+
+                if (obj.GetType() == typeof(double))
+                {
+                    sum += (double)obj;
+                }
+
+                if (obj.GetType() == typeof(int))
+                {
+                    total += (int)obj;
+                }
+
+                if (obj.GetType() == typeof(string))
+                {
+                    sentance+= (string)obj;
+                }
+            }
+
+            Console.WriteLine("Print out sum");
+
+            
+
+            Console.WriteLine($"Sum of ints: {total}");
+            Console.WriteLine($"Sum of doubles: {sum}");
+            Console.WriteLine($"Sum of Strings: {sentance}");
+
+            Console.ReadLine();
+        }
+    }
+}
